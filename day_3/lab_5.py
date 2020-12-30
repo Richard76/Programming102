d8e3cb998781ad96d77e8f870acce5bf2518caaa
Richard Farr <RichardFarr7@gmail.com>
2020-12-30 14:41:33 -0800

    lab5

--- day_3/lab_5.py
+++ day_3/lab_5.py
@@ -13,12 +13,6 @@ This will make your life easier. You may also want to consider creating separate
 so you can refer back to previous working versions.
 '''
 
-profile = {
-    'gandalfTheGrey': 'noneShallPass!',
-    'Richard': 'Farr'
-}
-
-
 '''
 5.1
 Create a dictionary called profile
@@ -31,18 +25,50 @@ password - noneShallPass!
 Do not create your profile with the user's username as the key and their password as the value as below:
 
 'gandalfTheGrey':'noneShallPass'
+'''
+
+profile = {
+    # 'gandalfTheGrey': 'noneShallPass!',
+    'username': 'Richard',
+    'password': 'Farr'
+}
+
+
+'''
 Define a function called login() which will have parameters for:
 
 username_attempt
 password_attempt
-The login() function will return True if the values passed to the function for username_attempt and password_attempt match the values at the keys of username and password found in the profile.
+
+The login() function will return True if the values passed to the function for username_attempt and 
+password_attempt match the values at the keys of username and password found in the profile.
 
 If the credentials don't match those in the profile, the login() function will return False
 
 Create variables for a username_attempt and password_attempt which will emulate a user's login attempt.
 
 Pass the username_attempt and password_attempt to login() and tell the user whether their login attempt was successful.
+'''
 
+# print(profile.keys())
+
+'''
+def login(username_attempt, password_attempt):
+    if username_attempt in profile.keys() and password_attempt == profile[username_attempt]:
+        print("Good, your login was successful")
+        return True
+    else:
+        print("Sorry, at least one of those was incorrect")
+        return False
+
+username = input("What's your username? ")
+password = input("What's your password? ")
+
+login(username, password)
+'''
+
+
+'''
 Output:
 
 profile:
@@ -61,13 +87,54 @@ username: gandalfTheGrey
 password: myPrecious?
 
 Error! Your username or password was incorrect!
+'''
+
+
+'''
 5.2
 Create a REPL that asks the user for their username and password and attempts to log them in.
 
 If their login is successful, print a welcome message and end the program.
 
 If their login is unsuccessful, ask if they'd like to try again.
+'''
 
+
+
+def login(username_attempt, password_attempt):
+    if username_attempt == profile['username'] and password_attempt == profile['password']:
+        # print("Good, your login was successful")
+        return True
+    else:
+        # print("Sorry, at least one of those was incorrect")
+        return False
+
+
+login_attempts = 0
+
+# REPL
+while login_attempts < 3:
+    username = input("What's your username? ")
+    password = input("What's your password? ")
+    sucsess = login(username, password)
+    if sucsess:
+        print("Good, your login was successful")
+        break
+    else: 
+        print("Sorry, at least one of those was incorrect")
+        login_attempts = login_attempts + 1 
+        try_again = input("Would you like to try to login again? (Y/N) ")
+        if try_again == "N":
+            break   
+else:
+    print("Sorry, 3 failed login attempts is enough for one day. Try again tomorrow. bye.")
+
+
+'''
+
+
+'''
+'''
 5.3
 Allow the user three attempts to login. If they exceed three attempts without a successful login, tell the user and end the program
 
@@ -102,13 +169,20 @@ Add support for multiple users.
 
 A few things will need to change:
 
-Instead of one profile, you will need a list of profiles. Each profile be a dictionary containing key : value pairs with the keys of username and password.
+Instead of one profile, you will need a list of profiles. Each profile be a dictionary containing key : value pairs 
+with the keys of username and password.
 
 The login() function will require a third parameter, profile.
 
-When the user enters their username and password attempts, loop through the list of profiles, pass each profile one at time into the login() function along with the username_attempt and password_attempt.
+When the user enters their username and password attempts, loop through the list of profiles, pass each profile 
+one at time into the login() function along with the username_attempt and password_attempt.
 
-If the username_attempt and password_attempt match the values at the keys of username and password current profile, the login() function will return that True and that user will be logged in. Otherwise, it will return False.
+If the username_attempt and password_attempt match the values at the keys of username and password current profile, 
+the login() function will return that True and that user will be logged in. Otherwise, it will return False.
+
+
+
+
 
 5.5 - Advanced
 Define a function called create_user() which:
